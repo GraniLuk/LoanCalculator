f70720356a64e4b9cef13ddb5a5baf2837d53ca9
granica.lukasz <granica.lukasz@live.com>
2017-09-19 18:58:38 +0200

    Dodany szablon (yeti) oraz początki kontroletów

---
+++ LoanCalculator/App_Start/BoundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Optimization;
+
+namespace LoanCalculator
+ 
+{
+    public class BoundlesConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap-yeti.css",
+                "~/Content/site.css"));
+        }
+    }
+}
---
+++ LoanCalculator/Controllers/LoanController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoanCalculator.Data;
+using LoanCalculator.Data.Models;
+
+namespace LoanCalculator.Controllers
+{
+    public class LoanController : Controller
+    {
+        private readonly LoanDb _db;
+
+        public LoanController()
+        {
+            _db = new LoanDb();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Loan/Create
+        [HttpPost]
+        public ActionResult Create(Loan loan)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+        }
+
+    }
+}
